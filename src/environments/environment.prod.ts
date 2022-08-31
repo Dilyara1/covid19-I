dd6a9458e86d13c82bb6566ff085155f8af40695
Dilyara1 <tulegenovadilyara@gmail.com>
2022-08-31 12:58:04 +0600

    feat: firebase config added.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDtre2f9V5Sfa91wb3QLAAPGFm5ebza-Uk",
+    authDomain: "covid19-i.firebaseapp.com",
+    projectId: "covid19-i",
+    storageBucket: "covid19-i.appspot.com",
+    messagingSenderId: "196080579781",
+    appId: "1:196080579781:web:16d1cb0dd9ec485f516cac",
+    measurementId: "G-F9PXQPWVMX"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDtre2f9V5Sfa91wb3QLAAPGFm5ebza-Uk",
+    authDomain: "covid19-i.firebaseapp.com",
+    projectId: "covid19-i",
+    storageBucket: "covid19-i.appspot.com",
+    messagingSenderId: "196080579781",
+    appId: "1:196080579781:web:16d1cb0dd9ec485f516cac",
+    measurementId: "G-F9PXQPWVMX"
+  }
 };
 
 /*
